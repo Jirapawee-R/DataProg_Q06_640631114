9b369abede2cda9f47a82b3c9c95f9258a1bd7d6
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-09-01 15:28:13 +0700

    Data Frame: Sorting

--- main.py
+++ main.py
@@ -62,3 +62,8 @@ print(df_sub.loc[10:20, ['rank', 'sex', 'salary']])
 # %%
 # 11) Select range of rows using position use method iloc
 print(df_sub.iloc[10:20, [0,3,4,5]])
+
+# %%
+# 12) Create a new dataframe from orinal one sorted by service
+df_sort = df.sort_values(by='service')
+print(df_sort.head())
