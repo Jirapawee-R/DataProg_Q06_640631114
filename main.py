3851365bb4c01c6fbe244b8f26e102d1817f6a0c
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-09-01 14:56:23 +0700

    Initial version: read Salaries.csv

---
+++ main.py
@@ -0,0 +1,15 @@
+# %%
+import pandas as pd
+
+df = pd.read_csv("~/Desktop/MsD/DataProg/Salaries.csv")
+
+print(df.head())
+
+# HANDS-ON EXERCISES (QUIZ 1)
+# Read first 10, 20, 50 records
+print(df.head(10))
+print(df.head(20))
+print(df.head(50))
+
+# How to view the last few records
+print(df.tail())
